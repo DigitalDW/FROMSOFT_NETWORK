e1e4e23a371bda1d3725fe99a15d1aa115de209a
Loris Rimaz <loris.rimaz@gmail.com>
2024-09-26 11:00:44 +0200

    Can switch between original and directed graphs. Corrected directed edges data for ER

--- D3_network_graph/network.js
+++ D3_network_graph/network.js
@@ -1,7 +1,12 @@
 const width = window.innerWidth;
 const height = window.innerHeight - 120;
+let view_dir = true;
 
 d3.select("#select_game").on("change", selectGame);
+d3.select("#change_network_function").on("click", () => {
+  view_dir = !view_dir;
+  selectGame("reload");
+});
 
 const svg = d3
   .select("body")
@@ -44,10 +49,14 @@ function zoomed({ transform }) {
 }
 
 function selectGame(event) {
-  g.selectAll(".nodes").remove();
-  g.selectAll("line").remove();
-  const target = event.target;
-  const value = target.value;
+  let target;
+  let value;
+  if (event != "reload") {
+    target = event.target;
+    value = target.value;
+  } else {
+    value = d3.select("#select_game").node().value;
+  }
   switch (value) {
     case "DS1":
       loadData(value, "darksouls1");
@@ -76,7 +85,14 @@ function loadData(game, folder) {
       d3.json(edgesPath)
         .then((data) => {
           const edges = formatEdges(data);
-          startForceLayout(nodes, edges);
+          g.selectAll(".nodes").remove();
+          g.selectAll(".links").remove();
+          g.selectAll("g").remove();
+          if (view_dir) {
+            startForceLayoutDir(nodes, edges);
+          } else if (!view_dir) {
+            startForceLayout(nodes, edges);
+          }
         })
         .catch((error) => console.log("Error loading data:", error));
     })
@@ -98,7 +114,7 @@ function formatEdges(data) {
   return edgeData;
 }
 
-function startForceLayout(nodes, links) {
+function startForceLayoutDir(nodes, links) {
   const allZones = new Set(nodes.map((row) => row.zone));
   const nZones = allZones.size;
   const color = d3.scaleOrdinal(allZones, [
@@ -145,12 +161,16 @@ function startForceLayout(nodes, links) {
     .selectAll(".links")
     .data(links)
     .enter()
-    .append("line")
+    .append("path")
     .attr("stroke", "#999")
+    .attr("fill", "none")
     .attr("stroke-opacity", 0.5)
     .attr("stroke-width", 2)
     .attr("class", "links")
-    .attr("marker-end", "url(#arrowhead)"); //The marker-end attribute defines the arrowhead or polymarker that will be drawn at the final vertex of the given shape.
+    .attr("marker-end", "url(#arrowhead)")
+    .on("click", (event, d) => {
+      console.log(d.source.id, d.target.id);
+    }); //The marker-end attribute defines the arrowhead or polymarker that will be drawn at the final vertex of the given shape.
 
   // Initialize the nodes
   const node = g
@@ -173,7 +193,6 @@ function startForceLayout(nodes, links) {
     .attr("fill", (d) => color(d.zone));
 
   node.each(function (p, j) {
-    console.log(d3.select(this));
     const words = p.name.split(" ");
     words.forEach((word, idx) => {
       d3.select(this)
@@ -205,12 +224,25 @@ function startForceLayout(nodes, links) {
 
   // This function is run at each iteration of the force algorithm, updating the nodes position (the nodes data array is directly manipulated).
   function ticked() {
-    console.log("tick");
-    link
-      .attr("x1", (d) => d.source.x)
-      .attr("y1", (d) => d.source.y)
-      .attr("x2", (d) => d.target.x)
-      .attr("y2", (d) => d.target.y);
+    link.attr("d", function (d) {
+      var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = Math.sqrt(dx * dx + dy * dy);
+      return (
+        "M" +
+        d.source.x +
+        "," +
+        d.source.y +
+        "A" +
+        dr +
+        "," +
+        dr +
+        " 0 0,1 " +
+        d.target.x +
+        "," +
+        d.target.y
+      );
+    });
 
     node.attr("transform", (d) => `translate(${d.x},${d.y})`);
   }
@@ -236,123 +268,122 @@ function startForceLayout(nodes, links) {
     simulation.stop();
   }
 }
-// function startForceLayout(nodes, links) {
-//   const allZones = new Set(nodes.map((row) => row.zone));
-//   const nZones = allZones.size;
-//   const color = d3.scaleOrdinal(allZones, [
-//     "#c35921",
-//     "#6376e6",
-//     "#5eb847",
-//     "#c06ee5",
-//     "#a6b535",
-//     "#714dbb",
-//     "#cca238",
-//     "#a738a6",
-//     "#5bbe7f",
-//     "#c9348b",
-//     "#557f2f",
-//     "#e668c0",
-//     "#39855f",
-//     "#dd3e77",
-//     "#4cc2bc",
-//     "#d5482e",
-//     "#52a0d6",
-//     "#df8c31",
-//     "#4a69b0",
-//     "#acb064",
-//     "#a26fc2",
-//     "#7b712b",
-//     "#d895e0",
-//     "#995e2e",
-//     "#9598de",
-//     "#d63d4f",
-//     "#745194",
-//     "#da9a69",
-//     "#b35397",
-//     "#e27b6a",
-//     "#b571a2",
-//     "#a6454c",
-//     "#e996b7",
-//     "#894b67",
-//     "#db748b",
-//     "#a13f6a",
-//   ]);
+function startForceLayout(nodes, links) {
+  const allZones = new Set(nodes.map((row) => row.zone));
+  const nZones = allZones.size;
+  const color = d3.scaleOrdinal(allZones, [
+    "#c35921",
+    "#6376e6",
+    "#5eb847",
+    "#c06ee5",
+    "#a6b535",
+    "#714dbb",
+    "#cca238",
+    "#a738a6",
+    "#5bbe7f",
+    "#c9348b",
+    "#557f2f",
+    "#e668c0",
+    "#39855f",
+    "#dd3e77",
+    "#4cc2bc",
+    "#d5482e",
+    "#52a0d6",
+    "#df8c31",
+    "#4a69b0",
+    "#acb064",
+    "#a26fc2",
+    "#7b712b",
+    "#d895e0",
+    "#995e2e",
+    "#9598de",
+    "#d63d4f",
+    "#745194",
+    "#da9a69",
+    "#b35397",
+    "#e27b6a",
+    "#b571a2",
+    "#a6454c",
+    "#e996b7",
+    "#894b67",
+    "#db748b",
+    "#a13f6a",
+  ]);
 
-//   const linkValues = links.map((row) => row.value);
-//   const forceScale = d3.scaleLinear(
-//     [d3.min(linkValues), d3.max(linkValues)],
-//     [2.25, 0.2]
-//   );
+  const linkValues = links.map((row) => row.value);
+  const forceScale = d3.scaleLinear(
+    [d3.min(linkValues), d3.max(linkValues)],
+    [2.25, 0.2]
+  );
 
-//   const simulation = d3
-//     .forceSimulation(nodes)
-//     .force(
-//       "link",
-//       d3
-//         .forceLink(links)
-//         .id((d) => d.id)
-//         .strength((d) => forceScale(d.value))
-//     )
-//     .force("charge", d3.forceManyBody())
-//     .force("center", d3.forceCenter(width / 2, height / 2))
-//     .on("tick", ticked);
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force(
+      "link",
+      d3
+        .forceLink(links)
+        .id((d) => d.id)
+        .strength((d) => forceScale(d.value))
+    )
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", ticked);
 
-//   const link = g
-//     .append("g")
-//     .attr("class", "can_remove")
-//     .attr("stroke", "#999")
-//     .attr("stroke-opacity", 0.6)
-//     .selectAll()
-//     .data(links)
-//     .join("line")
-//     .attr("marker-end", "url(#arrowhead)")
-//     .attr("stroke-width", 3);
+  const link = g
+    .append("g")
+    .attr("class", "can_remove")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+    .selectAll()
+    .data(links)
+    .join("line")
+    .attr("stroke-width", (d) => Math.sqrt(d.value) + 3);
 
-//   const node = g
-//     .append("g")
-//     .attr("class", "can_remove")
-//     .attr("stroke", "#fff")
-//     .attr("stroke-width", 1.5)
-//     .selectAll()
-//     .data(nodes)
-//     .join("circle")
-//     .attr("r", 10)
-//     .on("mouseover", (e, d) => {
-//       console.log(d.name);
-//     })
-//     .attr("fill", (d) => color(d.zone));
+  const node = g
+    .append("g")
+    .attr("class", "can_remove")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 1.5)
+    .selectAll()
+    .data(nodes)
+    .join("circle")
+    .attr("r", 10)
+    .on("mouseover", (e, d) => {
+      console.log(d.name);
+    })
+    .attr("fill", (d) => color(d.zone));
 
-//   function ticked() {
-//     link
-//       .attr("x1", (d) => d.source.x)
-//       .attr("y1", (d) => d.source.y)
-//       .attr("x2", (d) => d.target.x)
-//       .attr("y2", (d) => d.target.y);
+  function ticked() {
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
 
-//     node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-//   }
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+  }
 
-//   node.call(
-//     d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
-//   );
+  node.call(
+    d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
+  );
 
-//   function dragstarted(event) {
-//     if (!event.active) simulation.alphaTarget(0.3).restart();
-//     event.subject.fx = event.subject.x;
-//     event.subject.fy = event.subject.y;
-//   }
+  function dragstarted(event) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  }
 
-//   function dragged(event) {
-//     event.subject.fx = event.x;
-//     event.subject.fy = event.y;
-//   }
+  function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  }
 
-//   function dragended(event) {
-//     if (!event.active) simulation.alphaTarget(0);
-//     event.subject.fx = null;
-//     event.subject.fy = null;
-//   }
-// }
+  function dragended(event) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
+  }
+}
 
 d3.select("#select_game").node().value = "DS1";
 loadData("DS1", "darksouls1");
