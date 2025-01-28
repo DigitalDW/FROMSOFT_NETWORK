960a3fbcc9ca632a6fe8a74569b11984376e5363
Loris Rimaz <loris.rimaz@gmail.com>
2025-01-28 11:11:28 +0100

    Refactoring
    Added possibility to display central nodes only in large lettering.

--- D3_network_graph/network.js
+++ D3_network_graph/network.js
@@ -1,6 +1,8 @@
 const width = window.innerWidth;
 const height = window.innerHeight - 120;
 let view_dir = true;
+let view_cent = false;
+let simulation;
 
 d3.select("#select_game").on("change", selectGame);
 d3.select("#change_network_function").on("click", (event) => {
@@ -22,6 +24,8 @@ const svg = d3
   .attr("style", "max-width: 100%; height: auto; border: 1px solid black;");
 
 const g = svg.append("g").attr("cursor", "grab");
+const links_group = g.append("g");
+const nodes_group = g.append("g");
 
 g.append("defs")
   .append("marker")
@@ -39,9 +43,6 @@ g.append("defs")
   .attr("opacity", 0.75)
   .style("stroke", "none");
 
-const saveToSVG = document.getElementById("saveToSVG");
-saveToSVG.addEventListener("click", (event) => exportToSVG(event, svg));
-
 const borderOnOff = document.getElementById("borderOnOff");
 borderOnOff.addEventListener("click", (event) => {
   if (event.target.value == "on") {
@@ -53,6 +54,17 @@ borderOnOff.addEventListener("click", (event) => {
     svg.attr("style", "border: 1px solid black;");
     event.target.innerHTML = "Turn border off";
   }
+});
+
+const saveToSVG = document.getElementById("saveToSVG");
+saveToSVG.addEventListener("click", (event) => exportToSVG(event, svg));
+
+const centralNodes = document.getElementById("centralNodes");
+centralNodes.addEventListener("click", (event) => {
+  view_cent = !view_cent;
+  event.target.innerHTML = "Display central nodes' labels";
+  if (view_cent) event.target.innerHTML = "Display all labels";
+  selectGame("reload");
 });
 
 svg.call(
@@ -107,14 +119,15 @@ function loadData(game, folder) {
       d3.json(edgesPath)
         .then((data) => {
           const edges = formatEdges(data);
-          g.selectAll(".nodes").remove();
-          g.selectAll(".links").remove();
-          g.selectAll("g").remove();
-          if (view_dir) {
-            startForceLayoutDir(nodes, edges);
-          } else if (!view_dir) {
-            startForceLayout(nodes, edges);
-          }
+          nodes_group.selectAll(".nodes").remove();
+          links_group.selectAll(".links").remove();
+          startForceLayout(nodes, edges);
+          //g.selectAll("g").remove();
+          //if (view_dir) {
+          //  startForceLayoutDir(nodes, edges);
+          //} else if (!view_dir) {
+          //  startForceLayout(nodes, edges);
+          //}
         })
         .catch((error) => console.log("Error loading data:", error));
     })
@@ -136,66 +149,31 @@ function formatEdges(data) {
   return edgeData;
 }
 
-function startForceLayoutDir(nodes, links) {
+function startForceLayout(nodes, links) {
   const allZones = new Set(nodes.map((row) => row.zone));
-  const nZones = allZones.size;
-  const color = d3.scaleOrdinal(allZones, [
-    "#c35921",
-    "#6376e6",
-    "#5eb847",
-    "#c06ee5",
-    "#a6b535",
-    "#714dbb",
-    "#cca238",
-    "#a738a6",
-    "#5bbe7f",
-    "#c9348b",
-    "#557f2f",
-    "#e668c0",
-    "#39855f",
-    "#dd3e77",
-    "#4cc2bc",
-    "#d5482e",
-    "#52a0d6",
-    "#df8c31",
-    "#4a69b0",
-    "#acb064",
-    "#a26fc2",
-    "#7b712b",
-    "#d895e0",
-    "#995e2e",
-    "#9598de",
-    "#d63d4f",
-    "#745194",
-    "#da9a69",
-    "#b35397",
-    "#e27b6a",
-    "#b571a2",
-    "#a6454c",
-    "#e996b7",
-    "#894b67",
-    "#db748b",
-    "#a13f6a",
-  ]);
+  const color = returnColor(allZones);
+  let forceScale;
 
+  const linkValues = links.map((row) => row.value);
+  forceScale = d3.scaleLinear(
+    [d3.min(linkValues), d3.max(linkValues)],
+    [0.5, 0.1]
+  );
   // Initialize the links
-  const link = g
+  const link = links_group
     .selectAll(".links")
     .data(links)
     .enter()
-    .append("path")
-    .attr("stroke", "#999")
-    .attr("fill", "none")
-    .attr("stroke-opacity", 0.5)
-    .attr("stroke-width", 4)
+    .append(view_dir ? "path" : "line")
     .attr("class", "links")
-    .attr("marker-end", "url(#arrowhead)")
-    .on("click", (event, d) => {
-      console.log(d.source.id, d.target.id);
-    }); //The marker-end attribute defines the arrowhead or polymarker that will be drawn at the final vertex of the given shape.
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.5)
+    .attr("stroke-width", view_dir ? 4 : (d) => Math.sqrt(d.value) + 3)
+    .attr("fill", "none")
+    .attr("marker-end", view_dir ? "url(#arrowhead)" : "none");
 
   // Initialize the nodes
-  const node = g
+  const node = nodes_group
     .selectAll(".nodes")
     .data(nodes)
     .enter()
@@ -207,7 +185,10 @@ function startForceLayoutDir(nodes, links) {
         .on("start", dragstarted) //start - after a new pointer becomes active (on mousedown or touchstart).
         .on("drag", dragged) //drag - after an active pointer moves (on mousemove or touchmove).
         .on("end", dragended) //end - after an active pointer becomes inactive (on mouseup, touchend or touchcancel).
-    );
+    )
+    .on("click", function () {
+      d3.select(this).moveToFront();
+    });
 
   node
     .append("circle")
@@ -215,18 +196,10 @@ function startForceLayoutDir(nodes, links) {
     .attr("fill", (d) => color(d.zone));
 
   node.each(function (p, j) {
-    const words = p.name.split(" ");
-    words.forEach((word, idx) => {
-      d3.select(this)
-        .append("text")
-        .attr("dy", idx + "em")
-        .attr("dx", -6)
-        .style("font-size", "8px")
-        .text(word);
-    });
+    displayNames(p, this);
   });
 
-  const simulation = d3
+  simulation = d3
     .forceSimulation()
     .alphaDecay(0)
     .force(
@@ -234,9 +207,10 @@ function startForceLayoutDir(nodes, links) {
       d3
         .forceLink() // This force provides links between nodes
         .id((d) => d.id) // This sets the node id accessor to the specified function. If not specified, will default to the index of a node.
-        .distance(30)
+        //.distance(30)
+        .strength((d) => forceScale(d.value))
     )
-    .force("charge", d3.forceManyBody().strength(-100)) // This adds repulsion (if it's negative) between nodes.
+    .force("charge", d3.forceManyBody().strength(view_dir ? -100 : -10)) // This adds repulsion (if it's negative) between nodes.
     .force("center", d3.forceCenter(width / 2, height / 2)) // This force attracts nodes to the center of the svg area
     .on("tick", ticked);
   //Listen for tick events to render the nodes as they update in your Canvas or SVG.
@@ -246,54 +220,79 @@ function startForceLayoutDir(nodes, links) {
 
   // This function is run at each iteration of the force algorithm, updating the nodes position (the nodes data array is directly manipulated).
   function ticked() {
-    link.attr("d", function (d) {
-      var dx = d.target.x - d.source.x,
-        dy = d.target.y - d.source.y,
-        dr = Math.sqrt(dx * dx + dy * dy);
-      return (
-        "M" +
-        d.source.x +
-        "," +
-        d.source.y +
-        "A" +
-        dr +
-        "," +
-        dr +
-        " 0 0,1 " +
-        d.target.x +
-        "," +
-        d.target.y
-      );
-    });
+    if (view_dir) {
+      link.attr("d", function (d) {
+        var dx = d.target.x - d.source.x,
+          dy = d.target.y - d.source.y,
+          dr = Math.sqrt(dx * dx + dy * dy);
+        return (
+          "M" +
+          d.source.x +
+          "," +
+          d.source.y +
+          "A" +
+          dr +
+          "," +
+          dr +
+          " 0 0,1 " +
+          d.target.x +
+          "," +
+          d.target.y
+        );
+      });
+    } else {
+      link
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
+    }
 
     node.attr("transform", (d) => `translate(${d.x},${d.y})`);
   }
-
-  //When the drag gesture starts, the targeted node is fixed to the pointer
-  //The simulation is temporarily “heated” during interaction by setting the target alpha to a non-zero value.
-  function dragstarted(event) {
-    if (event.active) simulation.alphaTarget(0.3).restart(); //sets the current target alpha to the specified number in the range [0,1].
-    event.subject.fy = event.subject.y; //fx - the node’s fixed x-position. Original is null.
-    event.subject.fx = event.subject.x; //fy - the node’s fixed y-position. Original is null.
-    simulation.restart();
-  }
-
-  //When the drag gesture starts, the targeted node is fixed to the pointer
-  function dragged(event) {
-    event.subject.fx = event.x;
-    event.subject.fy = event.y;
-  }
-  function dragended(event) {
-    if (!event.active) simulation.alphaTarget(0);
-    event.subject.fx = null;
-    event.subject.fy = null;
-    simulation.stop();
-  }
 }
-function startForceLayout(nodes, links) {
-  const allZones = new Set(nodes.map((row) => row.zone));
-  const nZones = allZones.size;
-  const color = d3.scaleOrdinal(allZones, [
+
+//When the drag gesture starts, the targeted node is fixed to the pointer
+//The simulation is temporarily “heated” during interaction by setting the target alpha to a non-zero value.
+function dragstarted(event) {
+  console.log(event.subject);
+  if (event.active) simulation.alphaTarget(0.3).restart(); //sets the current target alpha to the specified number in the range [0,1].
+  event.subject.fy = event.subject.y; //fx - the node’s fixed x-position. Original is null.
+  event.subject.fx = event.subject.x; //fy - the node’s fixed y-position. Original is null.
+  simulation.restart();
+}
+
+//When the drag gesture starts, the targeted node is fixed to the pointer
+function dragged(event) {
+  event.subject.fx = event.x;
+  event.subject.fy = event.y;
+}
+function dragended(event) {
+  if (!event.active) simulation.alphaTarget(0);
+  event.subject.fx = null;
+  event.subject.fy = null;
+  simulation.stop();
+}
+
+function exportToSVG(event, SVG) {
+  event.preventDefault();
+
+  SVG.attr("width", width);
+
+  const svgNode = SVG.node();
+  let data = new XMLSerializer().serializeToString(svgNode);
+
+  let svgBlob = new Blob([data], { type: "image/svg+xml;" });
+  let svgUrl = URL.createObjectURL(svgBlob);
+
+  let downloadLink = document.createElement("a");
+  downloadLink.href = svgUrl;
+  downloadLink.download = document.getElementById("select_game").value + ".svg";
+  downloadLink.click();
+}
+
+function returnColor(data) {
+  return d3.scaleOrdinal(data, [
     "#c35921",
     "#6376e6",
     "#5eb847",
@@ -331,116 +330,53 @@ function startForceLayout(nodes, links) {
     "#db748b",
     "#a13f6a",
   ]);
+}
 
-  const linkValues = links.map((row) => row.value);
-  const forceScale = d3.scaleLinear(
-    [d3.min(linkValues), d3.max(linkValues)],
-    [2.25, 0.2]
-  );
-
-  const simulation = d3
-    .forceSimulation(nodes)
-    .force(
-      "link",
-      d3
-        .forceLink(links)
-        .id((d) => d.id)
-        .strength((d) => forceScale(d.value))
-    )
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .on("tick", ticked);
-
-  const link = g
-    .append("g")
-    .attr("class", "can_remove")
-    .attr("stroke", "#999")
-    .attr("stroke-opacity", 0.5)
-    .selectAll()
-    .data(links)
-    .join("line")
-    .attr("stroke-width", (d) => Math.sqrt(d.value) + 3);
-
-  // Initialize the nodes
-  const node = g
-    .selectAll(".nodes")
-    .data(nodes)
-    .enter()
-    .append("g")
-    .attr("class", "nodes")
-    .call(
-      d3
-        .drag() //sets the event listener for the specified typenames and returns the drag behavior.
-        .on("start", dragstarted) //start - after a new pointer becomes active (on mousedown or touchstart).
-        .on("drag", dragged) //drag - after an active pointer moves (on mousemove or touchmove).
-        .on("end", dragended) //end - after an active pointer becomes inactive (on mouseup, touchend or touchcancel).
-    );
-
-  node
-    .append("circle")
-    .attr("r", 12) //+ d.runtime/20 )
-    .attr("fill", (d) => color(d.zone));
-
-  node.each(function (p, j) {
-    const words = p.name.split(" ");
+function displayNames(p, n) {
+  const words = p.name.split(" ");
+  if (!view_cent) {
     words.forEach((word, idx) => {
-      d3.select(this)
+      d3.select(n)
         .append("text")
         .attr("dy", idx + "em")
         .attr("dx", -6)
         .style("font-size", "8px")
+        //.style("font-weight", important_name ? "bold" : "normal")
         .text(word);
     });
+  } else {
+    let important_name = false;
+    if (
+      [
+        "Firelink Shrine",
+        "The Far Fire",
+        "Cathedral Ward",
+        "Table of Lost Grace",
+        "Hunter's Dream",
+        "Crucifixion Woods",
+      ].includes(p.name)
+    ) {
+      important_name = true;
+    }
+    if (important_name) {
+      words.forEach((word, idx) => {
+        d3.select(n)
+          .append("text")
+          .attr("dy", idx + "em")
+          .attr("dx", -6)
+          .style("font-size", "30px")
+          .text(word);
+      });
+    }
+  }
+}
+
+d3.selection.prototype.moveToFront = function () {
+  console.log(this);
+  return this.each(function () {
+    this.parentNode.appendChild(this);
   });
-
-  function ticked() {
-    link
-      .attr("x1", (d) => d.source.x)
-      .attr("y1", (d) => d.source.y)
-      .attr("x2", (d) => d.target.x)
-      .attr("y2", (d) => d.target.y);
-
-    node.attr("transform", (d) => `translate(${d.x},${d.y})`);
-  }
-
-  node.call(
-    d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
-  );
-
-  function dragstarted(event) {
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    event.subject.fx = event.subject.x;
-    event.subject.fy = event.subject.y;
-  }
-
-  function dragged(event) {
-    event.subject.fx = event.x;
-    event.subject.fy = event.y;
-  }
-
-  function dragended(event) {
-    if (!event.active) simulation.alphaTarget(0);
-    event.subject.fx = null;
-    event.subject.fy = null;
-  }
-}
-
-function exportToSVG(event, SVG) {
-  event.preventDefault();
-
-  SVG.attr("width", width);
-
-  const svgNode = SVG.node();
-  var data = new XMLSerializer().serializeToString(svgNode);
-
-  var svgBlob = new Blob([data], { type: "image/svg+xml;" });
-  var svgUrl = URL.createObjectURL(svgBlob);
-
-  var downloadLink = document.createElement("a");
-  downloadLink.href = svgUrl;
-  downloadLink.download = document.getElementById("select_game").value + ".svg";
-  downloadLink.click();
-}
+};
 
 d3.select("#select_game").node().value = "DS1";
 selectGame("reload");
